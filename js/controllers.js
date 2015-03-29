3d65bae1d7bc0bdb1038f7450102ab8af7c51451
Pat Hartl <pat@pathar.tl>
2015-03-28 23:54:47 -0500

    Added basic templating and routing. I think we're in motion now.

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,6 @@
-var statham = angular.module('statham', []);
+var statham = angular.module('statham', ['ngRoute']);
+
+var $html = {};
 
 statham.controller('StathamMainCtrl', function($scope, $http) {
 
@@ -9,4 +11,45 @@ statham.controller('StathamMainCtrl', function($scope, $http) {
 			$scope.site.info = data;
 		});
 
+	http = $http;
+
 });
+
+statham.controller('StathamHomeCtrl', function($scope, $http) {
+
+	http.jsonp('http://pathar.tl/wp-json/posts?_jsonp=JSON_CALLBACK').
+		success(function(data, status) {
+			$scope.posts = data;
+		});
+
+});
+
+statham.filter('rawHtml', ['$sce', function($sce){
+	return function(val) {
+		return $sce.trustAsHtml(val);
+	};
+}]);
+
+statham.config(function($routeProvider) {
+		$routeProvider
+			.when(
+				"/",
+					{
+						templateUrl: 'partials/front-page.html',
+						controller: 'StathamHomeCtrl'
+					}
+			)
+			.otherwise(
+				{
+					redirectTo: "/"
+				}
+			);
+	});
+
+function getPosts($scope, url) {
+	
+	http.jsonp(url).success(function(data, status) {
+		return data;
+	});
+
+}
