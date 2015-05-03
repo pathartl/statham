0f8819b5fea4e3621dec35e683f17e33d5b8d25d
Pat Hartl <pat@pathartl.me>
2015-05-03 16:22:08 -0500

    Created Sass files, updated Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,17 +11,36 @@ module.exports = function(grunt) {
 		},
 		watch: {
 			all: {
-				files: '*',
-				tasks: [''],
+				files: 'scss/style.scss',
+				tasks: ['sass', 'autoprefixer'],
 				options: {
 					debounceDelay: 250,
 				},
 			},
+		},
+		sass: {
+			dist: {
+				files: {
+					'build/style.css': 'scss/style.scss'
+				}
+			}
+		},
+		autoprefixer: {
+			single_file: {
+		    	options: {
+		    		browsers: ['last 2 versions', 'ie 9'],
+		    		map: true
+		    	},
+		    	src: 'build/style.css',
+		    	dest: 'style.css'
+		    },
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer')
 
 	grunt.registerTask('default', ['connect', 'watch']);
 
