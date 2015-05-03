aab32bc9272a4e5b34fec2c635ea37bf24bfe7c7
Pat Hartl <pat@pathartl.me>
2015-05-02 23:25:11 -0500

    Basic pagination!

--- js/controllers.js
+++ js/controllers.js
@@ -4,18 +4,34 @@ var http = {};
 
 var site_url = 'https://pathar.tl';
 
-statham.controller('StathamMainCtrl', function($scope, $http) {
+statham.controller('StathamMainCtrl', function($scope, $http, $routeParams) {
 
 	$scope.site = {};
+
+	$scope.postsPerPage = 3;
     
 	$http.get('https://pathar.tl/wp-json').
 		success(function(data, status) {
 			$scope.site.info = data;
 		});
 
+    $scope.numberOfPages = function() {
+    	if ( $scope.posts ) {
+        	return Array(Math.ceil($scope.posts.length/$scope.postsPerPage));
+        }
+    }
+
+    $scope.scrollToTop = function() {
+    	$("html, body").animate({ scrollTop: 0 }, "slow");
+    }
+
 });
 
-statham.controller('StathamHomeCtrl', function($scope, wordpress) {
+statham.controller('StathamHomeCtrl', function($scope, wordpress, $routeParams) {
+
+	if ( $routeParams.page > 0 ) {
+		$scope.currentPage = $routeParams.page - 1;
+	}
 
 	wordpress.getPosts('posts');
 
@@ -26,11 +42,6 @@ statham.controller('StathamSingleCtrl', function($scope, $routeParams, wordpress
 	$scope.slug = $routeParams.slug;
 
 	wordpress.getPosts( 'posts?filter[name]=' + $routeParams.slug );
-
-	// http.jsonp('http://pathar.tl/wp-json/posts?filter[name]=' + $routeParams.slug + '&_jsonp=JSON_CALLBACK').
-	//  	success(function(data, status) {
-	//  		$scope.post = data[0];
-	//  	});
 
 });
 
@@ -43,6 +54,15 @@ statham.filter('rawHtml', ['$sce', function($sce){
 		return $sce.trustAsHtml(val);
 	};
 }]);
+
+statham.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        if (input) {
+        	return input.slice(start);
+    	}
+    }
+});
 
 statham.factory('wordpress', function($http, $rootScope) {
 
@@ -57,7 +77,7 @@ statham.factory('wordpress', function($http, $rootScope) {
 			callback = '?_jsonp=JSON_CALLBACK';
 		}
 
-		$http.jsonp( site_url + '/wp-json/' + route + callback ).success(function(data) {
+		$http.jsonp( site_url + '/wp-json/' + route + callback + '&filter[posts_per_page]=-1' ).success(function(data) {
 			$rootScope.posts = data;
 		});
 
@@ -70,6 +90,13 @@ statham.config(function($routeProvider) {
 		$routeProvider
 			.when(
 				"/",
+					{
+						templateUrl: 'partials/front-page.html',
+						controller: 'StathamHomeCtrl'
+					}
+			)
+			.when(
+				"/page/:page",
 					{
 						templateUrl: 'partials/front-page.html',
 						controller: 'StathamHomeCtrl'
