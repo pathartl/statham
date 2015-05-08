e53b5e6faaaf94e20081d653702f8eb68e24af02
Pat Hartl <pat@pathartl.me>
2015-05-07 23:28:36 -0500

    Lightbox via Bootstrap modal

--- js/directives.js
+++ js/directives.js
@@ -4,6 +4,13 @@ statham.directive('lazyLoad', function () {
         restrict: 'A',
         link: function ($scope, element, attrs) {
             $scope.$watch('posts', function() {
+
+                $('.img-loader a').click(function(e) {
+                    e.preventDefault();
+                    $('#lightbox .modal-body').html('<img src="' + $(this).find('img').attr('src') + '">');
+                    $('#lightbox').modal('show');
+                });
+
                 $('img').unveil(0, function() {
                   $(this).load(function() {
                     this.style.opacity = 1;
@@ -13,7 +20,7 @@ statham.directive('lazyLoad', function () {
                 $('pre code').each(function(i, block) {
                     hljs.highlightBlock(block);
                 });
-                
+
             });
         }
     }
